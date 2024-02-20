efeacff9ac57ef7dd7430c08c4bf3f162f2c3fc5
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-02-20 10:12:26 -0300

    first commit

---
+++ src/test/java/br/com/marcos/devweek/DevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.devweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
